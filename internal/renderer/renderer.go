13b60ce58ea669d32bab8f5a854015ea6a8e71bb
Elliot <67360013+elliot40404@users.noreply.github.com>
2025-01-09 03:45:50 +0530

    Use huh to render the list

--- internal/renderer/renderer.go
+++ internal/renderer/renderer.go
@@ -1,93 +1,49 @@
 package renderer
 
 import (
-	"fmt"
 	"log/slog"
 	"os"
 
-	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/huh"
 
 	"github.com/elliot40404/modo/internal/parser"
 )
 
 type Renderer struct {
-	List []parser.Todo
-	File *os.File
-}
-
-type Model struct {
-	Renderer
-	cursor   int
-	selected map[int]struct{}
+	List     []parser.Todo
+	File     *os.File
+	Selected []int
 }
 
 func NewRenderer(list []parser.Todo, file *os.File) *Renderer {
 	return &Renderer{
-		List: list,
-		File: file,
+		List:     list,
+		File:     file,
+		Selected: make([]int, 0),
 	}
 }
 
 func (r Renderer) Render() {
-	selected := make(map[int]struct{})
+	options := make([]huh.Option[int], len(r.List))
 	for i, todo := range r.List {
-		if todo.Done {
-			selected[i] = struct{}{}
-		}
+		options = append(options, huh.NewOption(todo.Content, i).Selected(todo.Done))
 	}
-	model := Model{
-		Renderer: r,
-		selected: selected,
-	}
-	p := tea.NewProgram(model, tea.WithAltScreen())
-	_, err := p.Run()
+	out := make([]int, 0)
+	form := huh.NewForm(
+		huh.NewGroup(huh.NewMultiSelect[int]().
+			Title("Todos").
+			Options(
+				options...,
+			).
+			Height(10).
+			Value(&out)),
+	)
+	err := form.Run()
 	if err != nil {
 		slog.Error("something went wrong", "error", err)
+		return
 	}
-}
-
-func (m Model) Init() tea.Cmd {
-	return nil
-}
-
-func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	if msg, ok := msg.(tea.KeyMsg); ok {
-		switch msg.String() {
-		case "ctrl+c", "q":
-			return m, tea.Quit
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			}
-		case "down", "j":
-			if m.cursor < len(m.List)-1 {
-				m.cursor++
-			}
-		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			m.List[m.cursor].ToggleChecked(m.File)
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
-			}
-		}
+	for _, i := range out {
+		r.List[i].ToggleChecked(r.File)
 	}
-	return m, nil
-}
-
-func (m Model) View() string {
-	s := ""
-	for i, choice := range m.List {
-		cursor := " "
-		if m.cursor == i {
-			cursor = ">"
-		}
-		checked := " "
-		if _, ok := m.selected[i]; ok {
-			checked = "x"
-		}
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice.Content)
-	}
-	return s
 }
